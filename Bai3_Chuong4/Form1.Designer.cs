ed4d854c9581e438601e5c3a2a073d97517eb3ee
TranHuynhHoaPhuc <6551071066@st.utc2.edu.vn>
2026-09-21 11:00:46 +0700

    Add files via upload

---
+++ Bai3_Chuong4/Form1.Designer.cs
@@ -0,0 +1,141 @@
+namespace ToDoListDemo
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        private System.Windows.Forms.TextBox txtCongViecMoi;
+        private System.Windows.Forms.Button btnThem;
+        private System.Windows.Forms.ListBox lstCongViec;
+        private System.Windows.Forms.Panel pnlTop;
+
+        private System.Windows.Forms.ContextMenuStrip cmsCongViec;
+        private System.Windows.Forms.ToolStripMenuItem mnuHoanThanh;
+        private System.Windows.Forms.ToolStripMenuItem mnuXoaMotCongViec;
+        private System.Windows.Forms.ToolStripMenuItem mnuXoaTatCa;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            pnlTop = new System.Windows.Forms.Panel();
+            btnThem = new System.Windows.Forms.Button();
+            txtCongViecMoi = new System.Windows.Forms.TextBox();
+            lstCongViec = new System.Windows.Forms.ListBox();
+            cmsCongViec = new System.Windows.Forms.ContextMenuStrip(components);
+            mnuHoanThanh = new System.Windows.Forms.ToolStripMenuItem();
+            mnuXoaMotCongViec = new System.Windows.Forms.ToolStripMenuItem();
+            mnuXoaTatCa = new System.Windows.Forms.ToolStripMenuItem();
+            label1 = new System.Windows.Forms.Label();
+            pnlTop.SuspendLayout();
+            cmsCongViec.SuspendLayout();
+            SuspendLayout();
+            // 
+            // pnlTop
+            // 
+            pnlTop.Controls.Add(btnThem);
+            pnlTop.Controls.Add(txtCongViecMoi);
+            pnlTop.Dock = System.Windows.Forms.DockStyle.Top;
+            pnlTop.Location = new System.Drawing.Point(0, 0);
+            pnlTop.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            pnlTop.Name = "pnlTop";
+            pnlTop.Padding = new System.Windows.Forms.Padding(14, 17, 14, 17);
+            pnlTop.Size = new System.Drawing.Size(714, 83);
+            pnlTop.TabIndex = 0;
+            // 
+            // btnThem
+            // 
+            btnThem.Location = new System.Drawing.Point(557, 22);
+            btnThem.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            btnThem.Name = "btnThem";
+            btnThem.Size = new System.Drawing.Size(129, 42);
+            btnThem.TabIndex = 1;
+            btnThem.Text = "Thêm";
+            btnThem.UseVisualStyleBackColor = true;
+            btnThem.Click += btnThem_Click;
+            // 
+            // txtCongViecMoi
+            // 
+            txtCongViecMoi.Location = new System.Drawing.Point(14, 23);
+            txtCongViecMoi.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            txtCongViecMoi.Name = "txtCongViecMoi";
+            txtCongViecMoi.Size = new System.Drawing.Size(527, 31);
+            txtCongViecMoi.TabIndex = 0;
+            // 
+            // lstCongViec
+            // 
+            lstCongViec.ContextMenuStrip = cmsCongViec;
+            lstCongViec.Dock = System.Windows.Forms.DockStyle.Fill;
+            lstCongViec.ItemHeight = 25;
+            lstCongViec.Location = new System.Drawing.Point(0, 83);
+            lstCongViec.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            lstCongViec.Name = "lstCongViec";
+            lstCongViec.Size = new System.Drawing.Size(714, 500);
+            lstCongViec.TabIndex = 1;
+            // 
+            // cmsCongViec
+            // 
+            cmsCongViec.ImageScalingSize = new System.Drawing.Size(24, 24);
+            cmsCongViec.Items.AddRange(new System.Windows.Forms.ToolStripItem[] { mnuHoanThanh, mnuXoaMotCongViec, mnuXoaTatCa });
+            cmsCongViec.Name = "cmsCongViec";
+            cmsCongViec.Size = new System.Drawing.Size(257, 100);
+            // 
+            // mnuHoanThanh
+            // 
+            mnuHoanThanh.Name = "mnuHoanThanh";
+            mnuHoanThanh.Size = new System.Drawing.Size(256, 32);
+            mnuHoanThanh.Text = "Đánh dấu hoàn thành";
+            mnuHoanThanh.Click += mnuHoanThanh_Click;
+            // 
+            // mnuXoaMotCongViec
+            // 
+            mnuXoaMotCongViec.Name = "mnuXoaMotCongViec";
+            mnuXoaMotCongViec.Size = new System.Drawing.Size(256, 32);
+            mnuXoaMotCongViec.Text = "Xóa công việc này";
+            mnuXoaMotCongViec.Click += mnuXoaMotCongViec_Click;
+            // 
+            // mnuXoaTatCa
+            // 
+            mnuXoaTatCa.Name = "mnuXoaTatCa";
+            mnuXoaTatCa.Size = new System.Drawing.Size(256, 32);
+            mnuXoaTatCa.Text = "Xóa tất cả";
+            mnuXoaTatCa.Click += mnuXoaTatCa_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new System.Drawing.Point(321, 312);
+            label1.Name = "label1";
+            label1.Size = new System.Drawing.Size(112, 25);
+            label1.TabIndex = 2;
+            label1.Text = "6551071066";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            ClientSize = new System.Drawing.Size(714, 583);
+            Controls.Add(label1);
+            Controls.Add(lstCongViec);
+            Controls.Add(pnlTop);
+            Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            Name = "Form1";
+            Text = "Danh sách việc cần làm hằng ngày";
+            pnlTop.ResumeLayout(false);
+            pnlTop.PerformLayout();
+            cmsCongViec.ResumeLayout(false);
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ Bai4_Chuong4/BTCh4_Bai4/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace BTCh4_Bai4
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            menuStrip1 = new MenuStrip();
+            nghiệpVụToolStripMenuItem = new ToolStripMenuItem();
+            menuBenhNhan = new ToolStripMenuItem();
+            menuLichHen = new ToolStripMenuItem();
+            menuCuaSo = new ToolStripMenuItem();
+            label1 = new Label();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.ImageScalingSize = new Size(20, 20);
+            menuStrip1.Items.AddRange(new ToolStripItem[] { nghiệpVụToolStripMenuItem, menuCuaSo });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.MdiWindowListItem = menuCuaSo;
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Padding = new Padding(8, 2, 0, 2);
+            menuStrip1.Size = new Size(1000, 33);
+            menuStrip1.TabIndex = 1;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // nghiệpVụToolStripMenuItem
+            // 
+            nghiệpVụToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { menuBenhNhan, menuLichHen });
+            nghiệpVụToolStripMenuItem.Name = "nghiệpVụToolStripMenuItem";
+            nghiệpVụToolStripMenuItem.Size = new Size(110, 29);
+            nghiệpVụToolStripMenuItem.Text = "Nghiệp vụ";
+            // 
+            // menuBenhNhan
+            // 
+            menuBenhNhan.Name = "menuBenhNhan";
+            menuBenhNhan.Size = new Size(279, 34);
+            menuBenhNhan.Text = "Thông tin bệnh nhân";
+            menuBenhNhan.Click += menuBenhNhan_Click;
+            // 
+            // menuLichHen
+            // 
+            menuLichHen.Name = "menuLichHen";
+            menuLichHen.Size = new Size(279, 34);
+            menuLichHen.Text = "Đặt lịch hẹn";
+            menuLichHen.Click += menuLichHen_Click;
+            // 
+            // menuCuaSo
+            // 
+            menuCuaSo.Name = "menuCuaSo";
+            menuCuaSo.Size = new Size(83, 29);
+            menuCuaSo.Text = "Cửa sổ";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(199, 238);
+            label1.Name = "label1";
+            label1.Size = new Size(112, 25);
+            label1.TabIndex = 3;
+            label1.Text = "6551071066";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1000, 562);
+            Controls.Add(label1);
+            Controls.Add(menuStrip1);
+            IsMdiContainer = true;
+            MainMenuStrip = menuStrip1;
+            Margin = new Padding(4, 4, 4, 4);
+            Name = "Form1";
+            Text = "Hệ thống quản lý phòng khám";
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem nghiệpVụToolStripMenuItem;
+        private ToolStripMenuItem menuBenhNhan;
+        private ToolStripMenuItem menuLichHen;
+        private ToolStripMenuItem menuCuaSo;
+        private Label label1;
+    }
+}
